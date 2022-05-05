56c1f78da622c291188cf4536bb7ced5833f74f1
LST <wer1264@gmail.com>
2022-05-05 23:53:04 +0900

    fix: 쿠키 속성 수정 (httpOnly)

--- src/controller/kakaoController.js
+++ src/controller/kakaoController.js
@@ -1,7 +1,9 @@
 const kakaoLogin = (req, res) => {
   const { accessToken, refreshToken } = req.user;
   res
-    .cookie('refresh_token', refreshToken)
+    .cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+    })
     .redirect(`${process.env.CLIENT_URL}/?token=${accessToken}`);
 };
 
