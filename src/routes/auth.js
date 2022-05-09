11718c93b4d83866184f553a2aaa1406093db890
LST <wer1264@gmail.com>
2022-05-09 14:36:27 +0900

    comment: auth router 주석 추가

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,8 +5,11 @@ const { kakaoLogin, googleLogin } = require('../controller/auth');
 
 const router = express.Router();
 
+// 카카오 로그인
 router.get('/kakao', passport.authenticate('kakao'));
 router.get('/kakao/callback', passport.authenticate('kakao'), kakaoLogin);
+
+// 구글 로그인
 router.get(
   '/google',
   passport.authenticate('google', {
