2680b57d0a97cc5a6674f7f903e85a2f6b7576be
LST <wer1264@gmail.com>
2022-05-21 22:43:50 +0900

    Update: 프로필 이미지 리사이징 처리

--- src/utils/service.js
+++ src/utils/service.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const sharp = require('sharp');
+
 const { bucket, storageBucket } = require('../firebase');
 
 function projectDataFormatChangeFn(projects) {
@@ -79,6 +81,23 @@ async function profileImageUploadFn(file, id, profileImage) {
       await deleteProfileImage(imageFilename);
     }
 
+    const image = await sharp(file.buffer);
+    const metadata = await image.metadata();
+
+    let resizedImage = null;
+
+    if (metadata.width > 640) {
+      resizedImage = await image
+        .resize({ width: 640 })
+        .withMetadata()
+        .toBuffer();
+    } else if (metadata.height > 640) {
+      resizedImage = await image
+        .resize({ height: 640 })
+        .withMetadata()
+        .toBuffer();
+    }
+
     await bucket
       .file(`profile/${filename}`)
       .createWriteStream({
@@ -86,7 +105,7 @@ async function profileImageUploadFn(file, id, profileImage) {
           contentType: file.mimetype,
         },
       })
-      .end(file.buffer);
+      .end(resizedImage === null ? file.buffer : resizedImage);
 
     const profileImageUrl = `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/profile%2F${filename}?alt=media`;
 
