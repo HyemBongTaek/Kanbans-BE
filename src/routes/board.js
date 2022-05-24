3ef8232a47ded7fbeda325e07510e18cbc17feb2
bong <poseson92@naver.com>
2022-05-24 23:28:46 +0900

    feat: board 수정 삭제 라우터 추가

--- src/routes/board.js
+++ src/routes/board.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
 const { auth } = require('../middlewares/auth');
-const { createBoard, getBoard } = require('../controller/board');
+const {
+  getBoard,
+  createBoard,
+  updateBoard,
+  deleteBoard,
+} = require('../controller/board');
 
 const router = express.Router();
 
 router.get('/:project_id', auth, getBoard);
 router.post('/', auth, createBoard);
+router.patch('/:id', auth, updateBoard);
+router.delete('/:id', auth, deleteBoard);
 
 module.exports = router;
