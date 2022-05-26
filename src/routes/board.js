ff0563d045be586880ced40f773bdcc311ca3b8e
LST <wer1264@gmail.com>
2022-05-26 22:09:28 +0900

    Comment: Board route 주석 추가

--- src/routes/board.js
+++ src/routes/board.js
@@ -11,11 +11,13 @@ const { createCard, deleteCard } = require('../controller/card');
 
 const router = express.Router();
 
+// 보드 생성
 router.get('/:project_id', auth, getBoard);
 router.post('/', auth, createBoard);
 router.patch('/:id', auth, updateBoard);
 router.delete('/:id', auth, deleteBoard);
 
+// 카드 생성
 router.post('/:boardId/card', auth, createCard);
 router.delete('/:boardId/card/:cardId', auth, deleteCard);
 
