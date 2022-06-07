975ed3cb78aa541edada475dcee4f73ae783cba1
bong <poseson92@naver.com>
2022-06-07 15:27:58 +0900

    Add: task 조회, 수정 기능 추가

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
 const { auth } = require('../middlewares/auth');
-const { createTask, deleteTask } = require('../controller/task');
+const {
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require('../controller/task');
 
 const router = express.Router();
 
+router.get('/:cardId', auth, getTask);
 router.post('/', auth, createTask);
+router.patch('/:id', auth, updateTask);
 router.delete('/:id', auth, deleteTask);
 
 module.exports = router;
