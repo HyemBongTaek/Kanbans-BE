913e6cd4df991a0e6bcf899e3658e6bcbfd5a6b5
LST <wer1264@gmail.com>
2022-07-25 15:10:24 +0900

    Add: Board, Card 순서를 저장하기 위한 Redis 함수 추가

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -21,7 +21,55 @@ async function setUserProfile(userId, profile) {
   }
 }
 
+async function setBoardOrder(id, order) {
+  try {
+    await redisClient.setEx(`project:${id}:board-order`, 900, order);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+async function getBoardOrder(id) {
+  try {
+    const boardOrder = await redisClient.get(`project:${id}:board-order`);
+
+    if (!boardOrder) {
+      return null;
+    }
+
+    return boardOrder;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+async function setCardOrder(id, order) {
+  try {
+    await redisClient.setEx(`board:${id}:card-order`, 900, order);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+async function getCardOrder(id) {
+  try {
+    const cardOrder = await redisClient.get(`board:${id}:card-order`);
+
+    if (!cardOrder) {
+      return null;
+    }
+
+    return cardOrder;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
 module.exports = {
+  getBoardOrder,
+  getCardOrder,
   getUserProfile,
+  setBoardOrder,
+  setCardOrder,
   setUserProfile,
 };
